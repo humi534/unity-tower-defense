1c83fa16c95b54171c3f03f4b2b2640c03ba164b
humi534 <hugo.poncelet@gmail.com>
2024-03-31 22:02:06 +0200

    First stone of the project
    Set the project as a 3D unity project with the goal to create a tower defense game. So far, only a cube has been created, with a script called 'Enemy' with the Debug.log info on start. The goal is to make sure other members of the projects are able to download properly the project.

---
+++ unity-tower-defense/Assets/enemy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("We did it");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
